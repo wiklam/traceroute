e4ee970fdf2f619c027ec4471b5fd0ce79d39f02
wiklam <wiktorpilar99@gmail.com>
2020-03-24 13:41:22 +0100

    Added functions, which handle network functions error and function to compute ICMP checksum.

--- check.c
+++ check.c
@@ -1,7 +1,57 @@
 #include "header.h"
 
+u_int16_t compute_icmp_checksum (const void *buff, int length){ //function from lecture
+    u_int32_t sum;
+    const u_int16_t* ptr = buff;
+    assert (length % 2 == 0);
+    for (sum = 0; length > 0; length -= 2)
+        sum += *ptr++;
+    sum = (sum >> 16) + (sum & 0xffff);
+    return (u_int16_t)(~(sum + (sum >> 16)));
+}
+
+
+int Select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout){
+    int rv = select(nfds, readfds, writefds, exceptfds, timeout);
+    if(rv < 0){
+        fprintf(stderr, "Select error\n");
+        exit(EXIT_FAILURE);
+    }
+    return rv;
+}
+
+
+int Socket(int domain, int type, int protocol){
+    int rv = socket(domain, type, protocol);
+    if (rv < 0){
+        fprintf(stderr, "Socket error\n");
+        exit(EXIT_FAILURE);
+    }
+    return rv;
+}
+
+
+ssize_t Recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen){
+    ssize_t rv = recvfrom(sockfd, buf, len, flags, src_addr, addrlen);
+    if (rv < 0){
+        fprintf(stderr, "Recvfrom error\n");
+        exit(EXIT_FAILURE);
+    }
+    return rv;
+}
+
+
+ssize_t Sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen){
+    ssize_t rv = sendto(sockfd, buf, len, flags, dest_addr, addrlen);
+    if (rv < 0){
+        fprintf(stderr, "Sendto error\n");
+        exit(EXIT_FAILURE);
+    }
+    return rv;
+}
+
+
 void check_enter(int arg_num, char *table[]){
-    
     if(arg_num != 2){
         fprintf(stderr, "Number of arguments incorrect\n");
         exit(EXIT_FAILURE);
@@ -20,7 +70,7 @@ void check_enter(int arg_num, char *table[]){
         }
         
         else{
-            if(number_count == 0 || *ip != '.'){
+            if(number_count == 0 || *ip != '.'){ //invalid symbol or to many dots in ip address
                 fprintf(stderr, "Invalid address\n");
                 exit(EXIT_FAILURE);
             }
@@ -32,7 +82,7 @@ void check_enter(int arg_num, char *table[]){
         byte *= 10;
         byte += (int) (*ip - '0');
         
-        if(byte > 255){
+        if(byte > 255){ //too high number in ip address
             fprintf(stderr, "Invalid address\n");
             exit(EXIT_FAILURE);
         }
@@ -40,7 +90,7 @@ void check_enter(int arg_num, char *table[]){
         ip++;
     }
 
-    if(byte_count != 4){
+    if(byte_count != 4){ //incorrect number of bytes in ip address
         fprintf(stderr, "Invalid address\n");
         exit(EXIT_FAILURE);
     }
--- header.h
+++ header.h
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+//#include <sys/types.h>          /* See NOTES - not required but in use in historical implementations*/
+#include <sys/select.h>
+#include <sys/socket.h>
+
+//Functions in check.c - data and functions checked for corresctness
+int Select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout){
+
+u_int16_t compute_icmp_checksum (const void *buff, int length){ //function from lecture
+int Socket(int domain, int type, int protocol)
+ssize_t Recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen){
+ssize_t Sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
 
 void check_enter(int num, char *table[]);
 
+//Functions in network.c - network handle
 void traceroute(char *ip);
